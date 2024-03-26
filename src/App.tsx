a23f753c25ba69937e13581f714280421507757f
Dobrunia <dobkos@hotmail.com>
2024-03-26 05:55:00 +0300

    build 1

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Position } from './components/Position';
 function App() {
   return (
     <div className="pages_wrapper">
+      <img className='logo' src="/1.PNG" alt="" />
       <section className="page">
         <div className="default_header">
           <h2>НА КРАНЕ *</h2>
@@ -367,6 +368,338 @@ USA, 5.4%, 400 мл`}
             />
           </BlockSmall>
         </div>
+      </section>
+      <section className="page">
+        <div className="default_header">
+          <h2>НОН АЛКО</h2>
+          <div className="small"></div>
+        </div>
+        <div className="block_wrapper">
+          <Block header="">
+            <Position
+              name={`Dreamteam Sport Pilsner`}
+              cost={`350`}
+              description={`Лагер, 330 мл`}
+            />
+            <Position
+              name={`Jaws Pineapple \\ Passionfruit \\ Apricot`}
+              cost={`420`}
+              description={`Фруктовое, 500 мл`}
+            />
+            <Position
+              name={`Schneider Weisse TAP 3`}
+              cost={`420`}
+              description={`Пшеничное, 500 мл`}
+            />
+            <Position
+              name={`Bullevie Petit`}
+              cost={`420`}
+              description={`Сидр, 330 мл`}
+            />
+            <Position
+              name={`Big Drop Pine Trail`}
+              cost={`480`}
+              description={`Пэйл Эль, 330 мл`}
+            />
+            <Position
+              name={`Big Drop Galactic`}
+              cost={`480`}
+              description={`Темное, 330 мл`}
+            />
+            <Position
+              name={`Samuel Smith Brown Ale`}
+              cost={`500`}
+              description={`Янтарный эль, 330 мл`}
+            />
+          </Block>
+          <Block header="Б/А НАПИТКИ ХОЛОДНЫЕ">
+            <Position
+              name={`Скоби Проект П.С.`}
+              cost={`200`}
+              description={`Комбуча, 330мл`}
+            />
+            <Position
+              name={`Фрэш джус апельсин`}
+              cost={`300`}
+              description={`250 мл`}
+            />
+            <Position
+              name={`Эспрессо тоник`}
+              cost={`300`}
+              description={`На комбуче, 330 мл`}
+            />
+            <Position
+              name={`Кокосовая вода`}
+              cost={`350`}
+              description={`330 мл`}
+            />
+            <Position
+              name={`Лимонад Fritz`}
+              cost={`420`}
+              description={`330 мл`}
+            />
+          </Block>
+          <Block header="КОФЕ / ЧАЙ">
+            <Position name={`Эспрессо`} cost={`200`} description={`40 мл`} />
+            <Position name={`Американо`} cost={`200`} description={`200 мл`} />
+            <Position
+              name={`с молоком`}
+              cost={`300`}
+              description={`200/300 мл`}
+            />
+            <Position
+              name={`Фильтр кофе`}
+              cost={`200`}
+              description={`200 мл`}
+            />
+            <Position
+              name={`Чайник чая`}
+              cost={`300`}
+              description={`Черный / зеленый / иван-чай / фруктовый 500 мл`}
+            />
+          </Block>
+        </div>
+      </section>
+      <section className="page">
+        <div className="kitchen_header">
+          <h2>К У Х Н Я</h2>
+          <div>11:00-16:00</div>
+          <span>
+            Пожалуйста, сообщите нам, если у вас есть аллергия на какие-либо
+            продукты
+          </span>
+        </div>
+        <div className="block_wrapper">
+          <Block header="ЗАВТРАК">
+            <Position
+              name={`Овсяная каша`}
+              cost={`350`}
+              description={`c вишневым вареньем и бананом`}
+            />
+            <Position
+              name={`Омлет с колбаской`}
+              cost={`400`}
+              description={`нежный омлет с колбаской на гриле и луком сибулет`}
+            />
+            <Position
+              name={`Френч тост с бананом`}
+              cost={`480`}
+              description={`соленой карамелью и ананасом на гриле`}
+            />
+            <Position
+              name={`Тост с тунцом`}
+              cost={`500`}
+              description={`и скрэмблом`}
+            />
+            <Position
+              name={`Сырный сендвич`}
+              cost={`550`}
+              description={`с двумя видами сыра, ветчиной и глазуньей`}
+            />
+            <Position
+              name={`Бан с лососем`}
+              cost={`550`}
+              description={`свежевыпеченный бан со сливочным сыром и лососем`}
+            />
+            <Position
+              name={`Лосось с яйцом пашот`}
+              cost={`650`}
+              description={`на картофельных драниках с соусом голландез`}
+            />
+            <Position
+              name={`Банановый кекс`}
+              cost={`250`}
+              description={`с заварным кремом`}
+            />
+          </Block>
+          <Block header="И НЕ ТОЛЬКО">
+            <Position
+              name={`Пряная картошка фри`}
+              cost={`350`}
+              description={`с соусом на выбор: анчоусный / сливочный хойсин /
+              сырный / 999 островов`}
+            />
+            <Position
+              name={`Куриный бульон`}
+              cost={`350`}
+              description={`с куриными наггетсами`}
+            />
+            <Position
+              name={`Картофельный суп`}
+              cost={`390`}
+              description={`подается с хрустящими опятами`}
+            />
+            <Position
+              name={`Хот-Дог в булочке бан`}
+              cost={`390`}
+              description={`с сосиской из свинины, коул слоу и кимчи майонезом`}
+            />
+            <Position
+              name={`Классический бургер`}
+              cost={`690`}
+              description={`говяжья котлета на домашнем хлебе`}
+            />
+          </Block>
+        </div>
+      </section>
+      <section className="page">
+        <div className="kitchen_header">
+          <h2>К У Х Н Я</h2>
+          <div>16:00-1:30</div>
+          <span>
+            Пожалуйста, сообщите нам, если у вас есть аллергия на какие-либо
+            продукты
+          </span>
+        </div>
+        <div className="block_wrapper">
+          <Block header="СТАРТЕРЫ">
+            <Position
+              name={`Попкорн`}
+              cost={`250`}
+              description={`с соленой карамелью`}
+            />
+            <Position
+              name={`Ореховый микс`}
+              cost={`390`}
+              description={`в пряной глазури из стаута и кленового сиропа`}
+            />
+            <Position
+              name={`Оливки гиганти`}
+              cost={`490`}
+              description={`в оливковом масле и специях`}
+            />
+            <Position
+              name={`Мясная тарелка`}
+              cost={`750`}
+              description={`сальсича, бастурма, вяленная утка. Подается с тартином, горчицей и хрустящими огурчиками`}
+            />
+          </Block>
+          <Block header="СУПЫ">
+            <Position
+              name={`Куриный бульон`}
+              cost={`350`}
+              description={`с куриными наггетсами`}
+            />
+            <Position
+              name={`Картофельный суп`}
+              cost={`390`}
+              description={`подается с хрустящими опятами`}
+            />
+          </Block>
+          <Block header="ЗАКУСКИ">
+            <Position
+              name={`Пряная картошка фри`}
+              cost={`350`}
+              description={`с соусом на выбор: анчоусный / сливочный хойсин /
+              сырный / 999 островов`}
+            />
+            <Position
+              name={`Спайси Пиклз`}
+              cost={`350`}
+              description={`со сметаной и укропом`}
+            />
+            <Position
+              name={`Маринованная свекла с соусом кешью`}
+              cost={`360`}
+              description={`маринуется в уксусе из собственной комбучи`}
+            />
+            <Position
+              name={`Костный мозг`}
+              cost={`360`}
+              description={`на бородинском хлебе, карамелизированный в квасном сусле`}
+            />
+            <Position
+              name={`Страчателла с кабачками`}
+              cost={`390`}
+              description={`каперсами и цедрой лимона`}
+            />
+            <Position
+              name={`Сырная кукуруза`}
+              cost={`390`}
+              description={`в сырном соусе и панировке из начос и лука`}
+            />
+            <Position
+              name={`Моцарелла с клюквенным джемом`}
+              cost={`430`}
+              description={`в панировке из кукурузных хлопьев`}
+            />
+            <Position
+              name={`Салат “Цезарь”`}
+              cost={`440`}
+              description={`хрустящий айсберг, куриное бедро, соус цезарь и перечный сыр`}
+            />
+            <Position
+              name={`Хрустящие куриные бедра`}
+              cost={`440`}
+              description={`в чили масле, на тостовом хлебе. Подаются с коул-слоу`}
+            />
+            <Position
+              name={`Картофельные драники`}
+              cost={`640`}
+              description={`со сметаной и красной икрой`}
+            />
+          </Block>
+          <Block header="ГОРЯЧЕЕ">
+            <Position
+              name={`Пряные кабачки`}
+              cost={`390`}
+              description={`со специями и кинзой`}
+            />
+            <Position
+              name={`Цветная капуста`}
+              cost={`390`}
+              description={`c ореховым соусом и арахисом`}
+            />
+            <Position
+              name={`Хот-Дог в булочке бан`}
+              cost={`390`}
+              description={`с сосиской из свинины, коул слоу и кимчи майонезом`}
+            />
+            <Position
+              name={`Фалафель 2.0`}
+              cost={`440`}
+              description={`с хумусом, аджикой, йогуртом и питой`}
+            />
+            <Position
+              name={`Чили Чиз Фрайз`}
+              cost={`490`}
+              description={`с чили кон карне, сырным соусом, маринованными огурчиками и халапеньо`}
+            />
+            <Position
+              name={`Кесадилья`}
+              cost={`490`}
+              description={`с курицей, сыром, томатной сальсой и ананасом`}
+            />
+            <Position
+              name={`Фиш энд Чипс 2.0`}
+              cost={`660`}
+              description={`с картофелем фри и коуол-слоу`}
+            />
+            <Position
+              name={`Классический бургер`}
+              cost={`690`}
+              description={`говяжья котлета на домашнем хлебе`}
+            />
+          </Block>
+          <Block header="ДЕСЕРТЫ">
+            <Position
+              name={`Банановый кекс`}
+              cost={`250`}
+              description={`с заварным кремом`}
+            />
+            <Position
+              name={`Шоколадный брауни `}
+              cost={`390`}
+              description={`с соленой карамелью и фундуком`}
+            />
+            <Position
+              name={`Френч тост с бананом`}
+              cost={`430`}
+              description={`c соленой карамелью и ананасом на гриле`}
+            />
+          </Block>
+        </div>
+        <img style={{ width: '100px' }} src="/5.PNG" alt="" />
       </section>
       {/* <BlockSmall header={''} milliliters={''}></BlockSmall>
           <Block header=''></Block>
